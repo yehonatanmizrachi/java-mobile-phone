6ee0a53cea297b7656e48f7f32adee4b48a9e9ec
RotemDoar <rotemdoar17@gmail.com>
2021-04-21 11:51:01 +0300

    Change TestMobilePhone

--- app/src/TestMobilePhone.java
+++ app/src/TestMobilePhone.java
@@ -7,6 +7,10 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 public class TestMobilePhone {
+	
+	enum APP{
+		CONTACTS,SMS,DIARY,MEDIA
+	}
 
 	public static void main(String[] args) throws IOException {
 		App[] application = new App[4];
@@ -16,11 +20,29 @@ public class TestMobilePhone {
 		Image modifiedpic = pic2.getScaledInstance(390, 110, java.awt.Image.SCALE_SMOOTH);
 		pic = new ImageIcon(modifiedpic);
  		String[] buttons = { "CONTACTS", "SMS", "DIARY", "MEDIA"};
- 		int returnValue = JOptionPane.showOptionDialog(null, null,"PHONE",
+ 		int appChosen = JOptionPane.showOptionDialog(null, null,"PHONE",
  		JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE, pic, buttons,null);
- 		//System.out.println(returnValue);
- 		if(returnValue == 0) {
- 			application[0].run();
+ 		
+ 		// 0 == CONTACTS , 1 == SMS, 2 == DIARY, 3 == MEDIA , -1 == EXIT
+ 		
+ 		while(appChosen != -1) {
+ 			
+ 			if(appChosen == APP.CONTACTS.ordinal()) {
+ 				application[0].run();
+ 			}
+ 			
+ 			if(appChosen == APP.SMS.ordinal()) {
+ 				application[1].run();
+ 			}
+ 			
+ 			if(appChosen == APP.DIARY.ordinal()) {
+ 				application[2].run();
+ 			}
+ 			
+ 			if(appChosen == APP.MEDIA.ordinal()) {
+ 				application[3].run();
+ 			}
+	 		
  		}
 
 	}
