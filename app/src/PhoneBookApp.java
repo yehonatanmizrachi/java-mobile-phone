2da9759da33d26c8bdd4ff99cb3f67668dc4fb93
RotemDoar <rotemdoar17@gmail.com>
2021-05-11 19:50:26 +0300

    fix bugs2

--- app/src/PhoneBookApp.java
+++ app/src/PhoneBookApp.java
@@ -4,13 +4,10 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-//import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
-
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
-
 
 public class PhoneBookApp extends ContactsApp {
 
