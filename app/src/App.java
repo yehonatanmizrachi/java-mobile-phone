846db1b7005f29050a5e928eda7113cc02be922d
RotemDoar <rotemdoar17@gmail.com>
2021-05-12 13:10:33 +0300

    fix notes

--- app/src/App.java
+++ app/src/App.java
@@ -2,9 +2,14 @@ package src;
 
 import java.io.IOException;
 
+// interface App
 public interface App {
 	
+	// method that run the application. Implemented by the application classes.
 	public void run() throws IOException;	
+	
+	// method that return string with all the application content.
+	// Implemented by the application classes.
 	public String getAppContent();
 
 }
--- app/src/ContactsApp.java
+++ app/src/ContactsApp.java
@@ -3,13 +3,18 @@ package src;
 import java.io.IOException;
 import java.util.ArrayList;
 
+// abstract class
 public abstract class ContactsApp implements App{
 
+	// Array that includes all the contacts.
 	public static ArrayList<Contact> contacts = new ArrayList<Contact>();
 
+	// method that removed all the content related to specific contact.
+	// Implemented by the inheritance classes.
 	public abstract void contactRemoved(Contact contact);
 	public abstract void run() throws IOException;
 
+	// search contact from the ArrayList
 	protected Contact search(String name) {
 		for (Contact contact : ContactsApp.contacts) { 
 		    if (contact.getName().equals(name)) {	
