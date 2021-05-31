ac6c86c785361cd8fa4cfd869ed8db4be40cb50b
Yehonatan Mizrachi <ymizrachi5@gmail.com>
2021-05-31 20:05:14 +0300

    first commit

--- app/blackjack/BlackjackApp.java
+++ app/blackjack/BlackjackApp.java
@@ -6,13 +6,35 @@ import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
+import java.awt.FlowLayout;  
+import javax.swing.JButton;  
+import javax.swing.JFrame;  
+import javax.swing.JLabel;  
+import javax.swing.JPanel;  
 import src.App;
 
 
 public class BlackjackApp implements App{
 
 	public void run() {
+		startMenu();
+	}
+	
 
+	private void startMenu() {	 
+        JFrame frame = new JFrame("JFrame Example");  
+        JPanel panel = new JPanel();  
+        panel.setLayout(new FlowLayout());  
+        JLabel label = new JLabel("JFrame By Example");  
+        JButton button = new JButton();  
+        button.setText("Button");  
+        panel.add(label);  
+        panel.add(button);  
+        frame.add(panel);  
+        frame.setSize(200, 300);  
+        frame.setLocationRelativeTo(null);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        frame.setVisible(true); 	
 	}
 
 	@Override
--- app/src/TestMobilePhone.java
+++ app/src/TestMobilePhone.java
@@ -1,8 +1,14 @@
 package src;
+import java.awt.FlowLayout;
 import java.io.IOException;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
+import blackjack.BlackjackApp;
 import diary.DiaryApp;
 import media.MediaApp;
 import phoneBook.PhoneBookApp;
@@ -14,21 +20,13 @@ public class TestMobilePhone {
 		PHONE_BOOK,
 		SMS,
 		DIARY,
-		MEDIA
+		MEDIA,
+		BLACKJACK
 	}
 
-	public static void main(String[] args) throws IOException {		
-		// initialize the applications
-		App[] application = new App[APPS.values().length];
-		ContactsApp smsApp = new SmsApp();
-		ContactsApp diaryApp = new DiaryApp();
-		ContactsApp[] contactsApps = { smsApp, diaryApp };
-
-		application[APPS.PHONE_BOOK.ordinal()] = new PhoneBookApp(contactsApps);
-		application[APPS.SMS.ordinal()] = smsApp;
-		application[APPS.DIARY.ordinal()] = diaryApp;		
-		application[APPS.MEDIA.ordinal()] = new MediaApp();
-
+	public static void main(String[] args) throws IOException {	 
+		runPhone();
+		/*
 		// prepare the main screen elements
 		ImageIcon mainPicture = ToolsFuncs.getMainPicture("Pic/mainPic.png",450,110);
  		String[] buttons = { "CONTACTS", "SMS", "DIARY", "MEDIA","Print All"};
@@ -53,7 +51,44 @@ public class TestMobilePhone {
  		}
  		mainPicture = ToolsFuncs.getMainPicture("Pic/BYE.png",300,80);
  		JOptionPane.showMessageDialog(null, null, null, JOptionPane.INFORMATION_MESSAGE, mainPicture);
- 		
+ 		*/
+	}
+	
+	private static void runPhone() throws IOException {
+
+		// initialize the applications
+		App[] application = new App[APPS.values().length];
+		ContactsApp smsApp = new SmsApp();
+		ContactsApp diaryApp = new DiaryApp();
+		ContactsApp[] contactsApps = { smsApp, diaryApp };
+
+		application[APPS.PHONE_BOOK.ordinal()] = new PhoneBookApp(contactsApps);
+		application[APPS.SMS.ordinal()] = smsApp;
+		application[APPS.DIARY.ordinal()] = diaryApp;		
+		application[APPS.MEDIA.ordinal()] = new MediaApp();
+		application[APPS.BLACKJACK.ordinal()] = new BlackjackApp();
+
+		application[APPS.BLACKJACK.ordinal()].run();
+		
+		// run the screen GUI
+		startPhoneMainScreen();
+	}
+
+
+	private static void startPhoneMainScreen() {
+		JFrame frame = new JFrame("JFrame Example");  
+        JPanel panel = new JPanel();  
+        panel.setLayout(new FlowLayout());  
+        JLabel label = new JLabel("JFrame By Example");  
+        JButton button = new JButton();  
+        button.setText("Button");  
+        panel.add(label);  
+        panel.add(button);  
+        frame.add(panel);  
+        frame.setSize(200, 300);  
+        frame.setLocationRelativeTo(null);  
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+        frame.setVisible(true);
 	}
 }
 	
