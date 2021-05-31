36a18f67bbb72db87fee95fcf3059baaa85d333d
Yehonatan Mizrachi <ymizrachi5@gmail.com>
2021-06-01 02:18:27 +0300

    finish game menu

--- app/blackjack/BlackjackApp.java
+++ app/blackjack/BlackjackApp.java
@@ -1,6 +1,7 @@
 package blackjack;
 
 import src.ToolsFuncs;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,6 +10,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
@@ -38,21 +41,49 @@ public class BlackjackApp implements App{
 
 	
 	private void startMenu() throws IOException {
-		// frame
-		String IMAGE_PATH = "Pic/IPHONE.png";
-		int WIDTH = 340, HEIGHT = 740;
-		JPanel panel = new JPanel();
 		
+		// frame
+		String IMAGE_PATH = "Pic/Menu.png";
+		int WIDTH = 590, HEIGHT = 530;
+		JPanel panel = new JPanel();
+	
         JLabel label = new JLabel(new ImageIcon(ImageIO.read(new File(IMAGE_PATH))));
-        label.setBounds(0, 0, 1000, 1000);
-        JLabel label_test = new JLabel(new ImageIcon(ImageIO.read(new File("Pic/BYE.png"))));
-        label_test.setBounds(0, 0, 1000, 1000);
+        label.setBounds(0, 0, WIDTH, HEIGHT);
+//        JLabel label_test = new JLabel(new ImageIcon(ImageIO.read(new File("Pic/BYE.png"))));
+//        label_test.setBounds(0, 0, 1000, 1000);
         
-        frame.add(label);
-        frame.add(label_test);
+        panel.add(label);
+    
+        // buttons
+        int BUTTONS_COUNT = 3;
+        JButton[] buttons = new JButton[BUTTONS_COUNT];
+
+        // buttons layout
+        int BUTTON_WIDTH = 345, BUTTON_HEIGHT = 60, BUTTON_PADDING = 35; 
+        int INITIAL_X = 115, INITIAL_Y = 175	;
+        int[][] BUTTONS_LOCATIONS = new int[BUTTONS_COUNT][2];
+    	for (int i = 0; i < BUTTONS_COUNT; i++) {
+    		// set x
+    		BUTTONS_LOCATIONS[i][0] = INITIAL_X;
+    		// set y
+			BUTTONS_LOCATIONS[i][1] = INITIAL_Y + (BUTTON_HEIGHT + BUTTON_PADDING)*i;
+    	}
+        
+    	for (int index = 0; index < BUTTONS_COUNT; index++) {
+    		buttons[index] = new JButton();
+        	buttons[index].addActionListener(getAppButtonEventListener(index));
+        	buttons[index].setBounds(BUTTONS_LOCATIONS[index][0], BUTTONS_LOCATIONS[index][1], BUTTON_WIDTH, BUTTON_HEIGHT);
+        	buttons[index].setOpaque(false);
+        	buttons[index].setContentAreaFilled(false);
+        	buttons[index].setBorderPainted(false);
+        		        			
+        	label.add(buttons[index]);
+        }
+        
+        frame.add(panel);
         frame.setSize(WIDTH, HEIGHT);
         frame.setLocationRelativeTo(null);
-//        frame.setResizable(false);
+        frame.setResizable(false);
         	
         frame.add(panel);
         frame.setVisible(true);
@@ -68,5 +99,16 @@ public class BlackjackApp implements App{
 	}
 
 
+	private ActionListener getAppButtonEventListener(int index) {
+		return new ActionListener(){
+	       	 public void actionPerformed(ActionEvent evt){
+	       		 try {
+	       			 System.out.print(index);
+	       		 } catch (Exception e) {
+	       			 System.out.print("Error:" + e);
+					}
+	       	 }
+       }; 
+	}
 
 }
