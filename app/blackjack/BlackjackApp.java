d266c7d5044fda2cd929937b4f5ded1eca0a4c57
Yehonatan Mizrachi <ymizrachi5@gmail.com>
2021-05-31 18:03:42 +0300

    seperate code into packejes

---
+++ app/blackjack/BlackjackApp.java
@@ -0,0 +1,24 @@
+package blackjack;
+
+import src.ToolsFuncs;
+import java.util.ArrayList;
+
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+import src.App;
+
+
+public class BlackjackApp implements App{
+
+	public void run() {
+
+	}
+
+	@Override
+	public String getAppContent() {
+		return "";
+	}
+
+
+}
