b07276c2dc7e4bc1383615ff711f1ce7f5ef74c1
Yehonatan Mizrachi <ymizrachi5@gmail.com>
2021-06-01 18:03:26 +0300

    fix statistics buttons

--- app/blackjack/frontend/BlackjackStatistics.java
+++ app/blackjack/frontend/BlackjackStatistics.java
@@ -1,15 +1,18 @@
 package blackjack.frontend;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import blackjack.BlackjackApp;
-import blackjack.backend.GameManager;
+import blackjack.BlackjackApp.APP_WINDOWS;
 
 
 public class BlackjackStatistics extends BlackjackWindow{
@@ -33,11 +36,47 @@ public class BlackjackStatistics extends BlackjackWindow{
         m_frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         m_frame.setResizable(false);
         m_frame.setVisible(true);
+        
+        // info button
+        JButton returnButton = new JButton();
+        int RETURN_BUTTON_WIDTH = 40, RETURN_BUTTON_HEIGHT = 40, RETURN_BUTTON_X = 33, RETURN_BUTTON_Y = 425;
+        int INFO_PADD = 18;
+
+        // return button
+        JButton infoButton = new JButton();
+        infoButton.addActionListener(getButtonEventListener(STATISTICS_BUTTONS.INFO));
+        infoButton.setBounds(RETURN_BUTTON_X, RETURN_BUTTON_Y, RETURN_BUTTON_WIDTH, RETURN_BUTTON_HEIGHT);
+        infoButton.setOpaque(false);
+        infoButton.setContentAreaFilled(false);
+        infoButton.setBorderPainted(false);
+        background.add(infoButton);
+
+        returnButton.addActionListener(getButtonEventListener(STATISTICS_BUTTONS.RETURN));
+        returnButton.setBounds(RETURN_BUTTON_X + RETURN_BUTTON_WIDTH + INFO_PADD, RETURN_BUTTON_Y, RETURN_BUTTON_WIDTH, RETURN_BUTTON_HEIGHT);
+        returnButton.setOpaque(false);
+        returnButton.setContentAreaFilled(false);
+        returnButton.setBorderPainted(false);
+        background.add(returnButton);
+        
 	}
 	
-	private enum INFO_BUTTONS {
-		RETURN,
-		HOME
+	private ActionListener getButtonEventListener(STATISTICS_BUTTONS action) {
+		return new ActionListener(){
+	       	 public void actionPerformed(ActionEvent evt){
+	       		 m_frame.setVisible(false);
+	       		 if (action == STATISTICS_BUTTONS.INFO) {
+	       			m_app.startWindow(APP_WINDOWS.INFO);
+	       		 }
+	       		 else if (action == STATISTICS_BUTTONS.RETURN) {
+	       			m_app.startWindow(APP_WINDOWS.MENU);
+	       		 }
+	       	 }
+       }; 
+	}
+	
+	private enum STATISTICS_BUTTONS {
+		INFO,
+		RETURN
 	}
 
 }
