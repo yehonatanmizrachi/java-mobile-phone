f69f7f3c42c4a66b45b85700ba24548f06e9079b
Yehonatan Mizrachi <ymizrachi5@gmail.com>
2021-06-01 13:25:13 +0300

    pull develop

---
+++ app/blackjack/DemoGameApp.java
@@ -0,0 +1,12 @@
+package blackjack;
+
+import java.io.IOException;
+
+import org.json.JSONException;
+
+public class DemoGameApp {
+	/*public static void main(String[] args) throws IOException, JSONException {	
+		GameManager g = new GameManager();
+		System.out.println(g.buildJson());
+	}*/
+}
---
+++ app/blackjack/User.java
@@ -0,0 +1,75 @@
+package blackjack;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import java.util.Scanner;
+public class User extends Player {
+	private int id;
+	private double money;
+	private int wins; // the amount of wins in game
+	final double initial_money = 1000;
+	
+	public User(int id)
+	{
+		this.sumOfCards = 0;
+		this.id = id;
+		try {
+		      File myObj = new File("./app/blackjack/players/" + "player" + id + ".txt");
+		      Scanner myReader = new Scanner(myObj);
+		      int k = 0;
+		      while (myReader.hasNextLine()) {
+		        String data = myReader.nextLine();
+		        if (k == 0)
+		        {
+		        	this.money = Double.parseDouble(data);
+		        	k++;
+		        }
+		        else
+		        	this.wins = Integer.parseInt(data);
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		    	File yourFile = new File("./app/blackjack/players/" + "player" + id + ".txt");
+		    	try {
+					yourFile.createNewFile();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} // if file already exists will do nothing 
+		    	try {
+					FileOutputStream oFile = new FileOutputStream(yourFile, false);
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+		    	this.wins = 0;
+		    	this.money = initial_money;
+		    }
+		
+	}
+	
+	public double getMoney()
+	{
+		return this.money;
+	}
+	
+	public int getWins()
+	{
+		return this.wins;
+	}
+	
+	public void setMoney(double money)
+	{
+		this.money = money;
+	}
+
+	@Override
+	public void makeMove() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
