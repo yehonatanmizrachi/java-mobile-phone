22398f8c3287567308ef817e64a9f66890719b96
Yehonatan Mizrachi <ymizrachi5@gmail.com>
2021-06-04 01:45:01 +0300

    small update

--- app/blackjack/BlackjackApp.java
+++ app/blackjack/BlackjackApp.java
@@ -40,11 +40,11 @@ public class BlackjackApp implements App {
 		m_backend = new GameManager();
 		
 		m_windows = new BlackjackWindow[]{
-				new BlackjackMenu("BlackJack - Menu", APPS_SIZES[APP_WINDOWS.MENU.ordinal()][0], APPS_SIZES[APP_WINDOWS.MENU.ordinal()][1], APPS_BACKGROUDS[APP_WINDOWS.MENU.ordinal()], this),
-				new BlackjackTable("BlackJack - Table", APPS_SIZES[APP_WINDOWS.TABLE.ordinal()][0], APPS_SIZES[APP_WINDOWS.TABLE.ordinal()][1], APPS_BACKGROUDS[APP_WINDOWS.TABLE.ordinal()], this),
-				new BlackjackInfo("BlackJack - Info", APPS_SIZES[APP_WINDOWS.INFO.ordinal()][0], APPS_SIZES[APP_WINDOWS.INFO.ordinal()][1], APPS_BACKGROUDS[APP_WINDOWS.INFO.ordinal()], this),
-				new BlackjackStatistics("BlackJack - Statistics", APPS_SIZES[APP_WINDOWS.STATISTICS.ordinal()][0], APPS_SIZES[APP_WINDOWS.STATISTICS.ordinal()][1], APPS_BACKGROUDS[APP_WINDOWS.STATISTICS.ordinal()], this),
-				new BlackjackEndGame("BlackJack - End Game", APPS_SIZES[APP_WINDOWS.END_GAME.ordinal()][0], APPS_SIZES[APP_WINDOWS.END_GAME.ordinal()][1], APPS_BACKGROUDS[APP_WINDOWS.END_GAME.ordinal()], this),
+				new BlackjackMenu(APPS_TITLES[APP_WINDOWS.MENU.ordinal()], APPS_SIZES[APP_WINDOWS.MENU.ordinal()][0], APPS_SIZES[APP_WINDOWS.MENU.ordinal()][1], APPS_BACKGROUDS[APP_WINDOWS.MENU.ordinal()], this),
+				new BlackjackTable(APPS_TITLES[APP_WINDOWS.TABLE.ordinal()], APPS_SIZES[APP_WINDOWS.TABLE.ordinal()][0], APPS_SIZES[APP_WINDOWS.TABLE.ordinal()][1], APPS_BACKGROUDS[APP_WINDOWS.TABLE.ordinal()], this),
+				new BlackjackInfo(APPS_TITLES[APP_WINDOWS.INFO.ordinal()], APPS_SIZES[APP_WINDOWS.INFO.ordinal()][0], APPS_SIZES[APP_WINDOWS.INFO.ordinal()][1], APPS_BACKGROUDS[APP_WINDOWS.INFO.ordinal()], this),
+				new BlackjackStatistics(APPS_TITLES[APP_WINDOWS.STATISTICS.ordinal()], APPS_SIZES[APP_WINDOWS.STATISTICS.ordinal()][0], APPS_SIZES[APP_WINDOWS.STATISTICS.ordinal()][1], APPS_BACKGROUDS[APP_WINDOWS.STATISTICS.ordinal()], this),
+				new BlackjackEndGame(APPS_TITLES[APP_WINDOWS.END_GAME.ordinal()], APPS_SIZES[APP_WINDOWS.END_GAME.ordinal()][0], APPS_SIZES[APP_WINDOWS.END_GAME.ordinal()][1], APPS_BACKGROUDS[APP_WINDOWS.END_GAME.ordinal()], this),
 		};
 		
 		m_windowsStack = new Stack<BlackjackWindow>();
@@ -119,4 +119,11 @@ public class BlackjackApp implements App {
 			null
 	};
 
+	private String[] APPS_TITLES = {
+			"BlackJack - Menu",
+			"BlackJack - Table",
+			"BlackJack - Info",
+			"BlackJack - Statistics",
+			"BlackJack - End Game"
+	};
 }
